91fcda7fa2cc630ac614fda0b7f7b835f8987216
FenyAdria <fenyadriam@gmail.com>
2026-05-15 10:07:00 +0700

    modif

--- login.php
+++ login.php
@@ -12,6 +12,7 @@ if (isset($_POST['login'])) {
 
     if ($user && password_verify($password, $user['password'])) {
         $_SESSION['login'] = true;
+        $_SESSION['email'] = $user['email'];
         $_SESSION['role'] = $user['role'];
 
         if ($user['role'] == 'admin') {
